26655881cc539cb6115e0e2642c4c232fe201c4c
zacharybaca <zachary.baca@msn.com>
2024-06-29 23:16:12 -0500

    cleaned up code and added boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+import DiceBox from "./components/DiceBox";
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>I am the App Component</h1>
+      <DiceBox />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/DiceBox.jsx
@@ -0,0 +1,9 @@
+import "./DiceBox.css";
+
+export default function DiceBox() {
+    return (
+        <div>
+            <h1>I am the DiceBox component</h1>
+        </div>
+    )
+}
